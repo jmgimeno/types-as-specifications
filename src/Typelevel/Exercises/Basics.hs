9c3ffcbfc7cd7b46e08fb6835ca6a06024e1639b
Juan Manuel Gimeno Illa <jmgimeno@diei.udl.cat>
2020-07-09 18:55:46 +0200

    Solved exercise 5 Basics via polykinds

--- src/Typelevel/Exercises/Basics.hs
+++ src/Typelevel/Exercises/Basics.hs
@@ -61,7 +61,7 @@ testAnd4 = Refl :: And 'False 'False :~: 'False
 --   Implement the IfThenElse type family that returns the 'a' type if the 'c'
 --   type is 'True, and the 'b' type if the 'c' type is 'False
 ----------------------------------------
-type family IfThenElse (c :: Bool) a b where
+type family IfThenElse (c :: Bool) (a :: k) (b :: k) :: k where
   IfThenElse 'True  a _ = a
   IfThenElse 'False _ b = b
 
